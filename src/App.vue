075ceacc9fe5283167ae7efd78d31edf5260da17
tsukigahara <satsuki.tksm@gmail.com>
2022-12-14 17:33:31 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import AppCardgroup from './components/AppCardgroup.vue'
+</script>
+
+<template>
+  <header></header>
+  <main class="container">
+    <AppCardgroup />
+  </main>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
